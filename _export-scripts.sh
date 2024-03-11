e2a58dc396268a154bee502432dfe281d9b137fa
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-11 12:31:01 +0100

    feat: Implement exporting all scripts in a folder into bash_aliases.

--- _export-scripts.sh
+++ _export-scripts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#todo: register alias via script
 
-# for entered path of script call get alias and add it to bash_aliases
+INVISIBLE_SCRIPTS_PREFIX=_
 source $SCRIPT_DIR/print/_print-colored-lib.sh
 
 print() {
@@ -9,20 +8,49 @@ print() {
 }
 
 addAlias() {
-	# check if alias is already present by checking "alias $1"
 	if [ "$#" -ne 2 ]; then
 		echo "invalid number of arguments to add the alias. Need 2 got $#"
 		exit
 	fi
 
-	if grep -Fxq "$1" /home/coerschkes/.bash_aliases; then
-		echo "alias $1 already registered, skipping.."
+	alColored=$(printColored "purple" "$1")
+
+	if grep -Fq "$1" ~/.bash_aliases; then
+		echo "alias $alColored already registered, skipping.."
 	else
-		alColored=$(printColored "purple" "$1")
 		scriptColored=$(printColored "green" "$2")
-		#		echo "alias $1=$2" >> ~/.bash_aliases
+		echo "alias $1=$2" >>~/.bash_aliases
 		echo "alias $alColored added for script $scriptColored."
 	fi
+}
+
+getBaseName() {
+	if [ "$#" -ne 1 ]; then
+		echo "error: getBaseName() invalid number of args. need [1] got [$#]"
+		echo "usage: <path>"
+		exit 1
+	fi
+
+	ext=${1##*.}
+	echo $(basename $1 .$ext)
+}
+
+addAliasesForPath() {
+	if [ "$#" -ne 1 ]; then
+		echo "error: addAliasesForPath() invalid number of args. need [1] got [$#]"
+		echo 'usage: <path>'
+		exit 1
+	fi
+
+	folder_path=$1
+
+	for file in $folder_path/*; do
+		base_name=$(getBaseName $file)
+		if [[ $base_name != $INVISIBLE_SCRIPTS_PREFIX* ]]; then
+			script_alias=$($file alias)
+			addAlias $script_alias $file
+		fi
+	done
 }
 
 if [ "$#" -ne 1 ]; then
@@ -30,6 +58,4 @@ if [ "$#" -ne 1 ]; then
 	exit
 fi
 
-p=$1
-al=$($SCRIPT_DIR/util/_get-alias.sh $p)
-addAlias $al $p
+addAliasesForPath $1
