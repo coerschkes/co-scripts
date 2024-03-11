2a47a628c8c20d3aa9d578b4cd8f7a8f619b1969
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-11 14:25:09 +0100

    feat: Implment a script for importing all scripts that are not invisible.

---
+++ import-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+print() {
+	echo ""
+}
+
+alias() {
+	echo "is"
+}
+
+if [ "$#" -eq 1 ]; then
+	if [ "$1" == "print" ]; then
+		print
+	elif [ "$1" == "alias" ]; then
+		alias
+	fi
+	exit 1
+fi
+
+rm -f ~/.bash_aliases && touch ~/.bash_aliases
+
+import_script=$SCRIPT_DIR/util/_import-scripts.sh
+$import_script "$SCRIPT_DIR"
+$import_script "$SCRIPT_DIR/util"
+$import_script "$SCRIPT_DIR/print"
+
+
