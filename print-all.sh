f8a89286588b90adba4f5a9d337c28b7cbae7695
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-11 14:49:32 +0100

    feat: Add a script for printing all scripts.

---
+++ print-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+print() {
+	echo ""
+}
+
+alias() {
+	echo "ps"
+}
+
+if [ "$#" -eq 1 ]; then
+	if [ "$1" == "print" ]; then
+		print
+	elif [ "$1" == "alias" ]; then
+		alias
+	fi
+	exit 1
+fi
+
+
+print_script=$SCRIPT_DIR/print/_print-scripts.sh
+$print_script "$SCRIPT_DIR"
+$print_script "$SCRIPT_DIR/print"
+$print_script "$SCRIPT_DIR/util"
